ccd5245390a71a4adbd47ca059b0396029c6f135
XxspitxX <srspit06@gmail.com>
2020-02-02 15:33:32 -0500

    Finalizado la app

---
+++ src/app/Models/cotizacion.ts
@@ -0,0 +1,17 @@
+export class Cotizacion{
+    constructor(
+        public nombre : string,
+        public apellidos : string,
+        public tdocumento : string,
+        public numero : string,
+        public fecha : string,
+        public direccion : string,
+        public complemento: string,
+        public procedimientoa : string,
+        public procedimientob : string,
+
+        public descripcion : string
+
+    ){}
+
+}
--- src/app/Pages/cotizacion-cirugia/cotizacion-cirugia.component.ts
+++ src/app/Pages/cotizacion-cirugia/cotizacion-cirugia.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Tipo } from '../solicitud-medico/solicitud/solicitud.component';
 import  *  as  Proceso  from  '../../../assets/json/cirugias.json';
+import { Cotizacion } from 'src/app/Models/cotizacion';
+import { ToastrService } from 'ngx-toastr';
+import { NgForm } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cotizacion-cirugia',
@@ -8,10 +12,32 @@ import  *  as  Proceso  from  '../../../assets/json/cirugias.json';
   styleUrls: ['./cotizacion-cirugia.component.scss']
 })
 export class CotizacionCirugiaComponent implements OnInit {
-  mainCategory = {
-    title: 'abc',
-    id: 1
-  };
+  mainCategory = [{
+    title: 'Neurocirugía',
+    id: 1,  
+  },
+  {
+    title: 'Ortopedia y Traumatología',
+    id: 2,  
+  },
+  {
+    title: 'Cardiovascular',
+    id: 3,  
+  },
+  {
+    title: 'Cirugía General',
+    id: 4,  
+  },
+  {
+    title: 'Urología',
+    id: 4,  
+  },
+  {
+    title: 'Ginecología',
+    id: 5,
+  }
+]
+  ;
   subcategory = {
     title: 'xxx',
     parentId: 1
@@ -19,30 +45,124 @@ export class CotizacionCirugiaComponent implements OnInit {
 
   mainGroups = [
     {
-      title: 'abc',
+      title: 'Neurocirugía',
       id: 1
     },
     {
-      title: 'def',
+      title: 'Ortopedia y Traumatología',
       id: 2
+    },
+    {
+      title: 'Cardiovascular',
+      id: 3
+    },
+    {
+      title: 'Cirugía General',
+      id: 4
+    },
+    {
+      title: 'Urología',
+      id: 5
+    },
+    {
+      title: 'Ginecología',
+      id: 6
     }
+
   ]
 
   subCategories = [
     {
-      title: 'xxx',
+      title: 'Tumores Cerebrales',
       parentId: 1
     },
     {
-      title: 'yyy',
+      title: 'Hematomas Cerebrales',
       parentId: 1
     },
     {
-      title: 'zzz',
+      title: 'Aneurismas',
+      parentId: 1
+    },
+
+    {
+      title: 'Cirugía de mano y pie',
       parentId: 2
+    },
+    {
+      title: 'Osteosíntesis para tratamiento de fracturas esqueléticas',
+      parentId: 2
+    },
+    {
+      title: 'Cirugías de trauma y deformidades en columna',
+      parentId: 2
+    },
+
+    {
+      title: 'Revascularización miocárdica con y sin circulación extracorpórea',
+      parentId: 3
+    },
+    {
+      title: 'Cambios valvulares',
+      parentId: 3
+    },
+    {
+      title: 'Aneurismas con injertos',
+      parentId: 3
+    },
+    {
+      title: 'Corrección de estenosis pulmonares',
+      parentId: 3
+    },
+    {
+      title: 'Corrección defectos congénitos cardíacos en niños',
+      parentId: 3
+    },
+    {
+      title: 'Resecciones intestinales',
+      parentId: 4
+    },
+    {
+      title: 'Tiroidectomías',
+      parentId: 4
+    },
+    {
+      title: 'Cirugía variada para tumores en diversas localizaciones',
+      parentId: 4
+    },
+    {
+      title: 'Procedimientos endoscópicos para cirugía antirreflujo y cirugía de obesidad',
+      parentId: 4
+    },
+    {
+      title: 'Nefrectomía',
+      parentId: 5
+    },
+    {
+      title: 'Recanalización de canal deferente',
+      parentId: 5
+    },
+    {
+      title: 'Histerectomía abdominal o vaginal',
+      parentId: 6
+    },
+    {
+      title: 'Quiste de ovario',
+      parentId: 6
+    },
+    {
+      title: 'Protocolo de ovario (Cirugía de cáncer)',
+      parentId: 6
+    },
+    {
+      title: 'Microcirugía tubárica y cirugías de fertilidad',
+      parentId: 6
     }
+
+
   ]
-  constructor() { }
+  constructor(private toast: ToastrService) { }
+  datos = new Cotizacion('', '' , '','','','','','','','');
   tipo: Tipo[] = [
     {value: 'TI', viewValue:'TI'},
     {value: 'CC',viewValue:'CC'},
@@ -53,5 +173,15 @@ export class CotizacionCirugiaComponent implements OnInit {
  filterSubById(id) {
     return this.subCategories.filter(item => item.parentId === id);
 }
+
+ 
   ngOnInit() {}
+
+
+  onSubmit(datos: NgForm, mainCategory : NgForm, subCategories: NgForm){
+    this.toast.success("Cotización Enviada se envia información al correo electronico");
+    datos.resetForm();
+    mainCategory.resetForm();
+    subCategories.resetForm();
+  }
 }
